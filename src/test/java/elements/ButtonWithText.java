da1a817499c0c6ebf34073f3c83f4ff012e1dfd9
svetlana <svetlana_040285@mail.ru>
2023-09-18 00:03:33 +0300

    added uploadphotoprofile

--- src/test/java/elements/ButtonWithText.java
+++ src/test/java/elements/ButtonWithText.java
@@ -1,3 +1,5 @@
+package elements;
+
 import lombok.extern.log4j.Log4j2;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
--- src/test/java/pages/SettingsNotificationPrivacyPage.java
+++ src/test/java/pages/SettingsNotificationPrivacyPage.java
@@ -78,4 +78,3 @@ public class SettingsNotificationPrivacyPage extends BasePage {
 
 
 }
-
